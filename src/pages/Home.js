ee40313818f263567f686289a58c43636b40e965
nadamur <nmurad4@uwo.ca>
2024-01-17 20:38:52 -0500

    home page simax

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,37 +1,62 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/Home.css';
-//banner images
+// banner images
 import BannerImage1 from '../assets/asl_4_2.jpg';
-import BannerImage2 from '../assets/asl_5.png'
+import BannerImage2 from '../assets/background.jpg';
+//simax images
+import HelloGIF from '../assets/hello_gif.gif';
+import HelloImage from '../assets/hello.png';
+import WonderfulGIF from '../assets/wonderful_gif.gif'
 
 function Home() {
-  //use state to alternate between banner images
-  const [currentImage, setCurrentImage] = useState(0);
-  //array for banner images
-  const images = [BannerImage1,BannerImage2];
+  //simax image
+  const [currentHelloImage, setCurrentHelloImage] = useState(HelloImage);
+  const [gifPlaying, setGifPlaying] = useState(false);
+  //say 'hello' on launch
   useEffect(() =>{
-    //change background every 2 seconds
-    const interval = setInterval(()=>{
-      setCurrentImage((prevIndex)=>(prevIndex+1)%images.length)
-    }, 6000);
-    return () => clearInterval(interval);
-  }, [images]);
-  const backgroundImageStyle ={
-    backgroundImage: `url(${images[currentImage]})`,
-    transition: 'background-image 1s ease-in-out',
-  }
+    changeHelloState();
+  }, []);
+  //play 'hello' gif
+  const changeHelloState = () => {
+    setGifPlaying(true);
+    setCurrentHelloImage(HelloGIF);
+
+    setTimeout(() => {
+      setGifPlaying(false);
+      setCurrentHelloImage(HelloImage);
+    }, 2000); // 2000 milliseconds = 2 seconds
+  };
+  //play 'wonderful' gif
+  const changeWonderfulState = () => {
+    setGifPlaying(true);
+    setCurrentHelloImage(WonderfulGIF);
+
+    setTimeout(() => {
+      setGifPlaying(false);
+      setCurrentHelloImage(HelloImage);
+    }, 2550); // 2000 milliseconds = 2 seconds
+  };
+
   return (
-    <div className='home' style={backgroundImageStyle}>
-        <div className='headerContainer'>
-            <h1>Hands-On Learning</h1>
-            <p>Where Learning Meets Fun!</p>
-            <Link to="/learn">
-                <button>GET STARTED!</button>
-            </Link>
+    <div className='home' style={{ backgroundColor: 'rgb(77, 41, 169)'}}>
+      <div className='headerContainer'>
+        <div className='leftSide'>
+          <img
+            src={currentHelloImage}
+            alt='Hello ASL Sign'
+            style={{ display: gifPlaying ? 'block' : 'none' }}
+          />
+          <img src = {currentHelloImage} style={{ display: gifPlaying ? 'none' : 'block' }} />
         </div>
+        <div className='rightSide'>
+          <h1>Hands-On Learning</h1>
+          <p>Where Learning Meets Fun!</p>
+          <button onClick={changeWonderfulState}>GET STARTED!</button>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
