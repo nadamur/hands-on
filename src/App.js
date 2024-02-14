366d40fec3791ac390e9cb45496a7cc74de8f3cb
nadamur <nmurad4@uwo.ca>
2024-02-13 20:21:38 -0500

    got rid of contact

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Home from './pages/Home';
 import Footer from './components/Footer';
 import Learn from './pages/Learn';
-import Contact from './pages/Contact';
 import Lessons from './pages/Lessons';
 import Translator from './pages/Translator';
 
@@ -16,7 +15,6 @@ function App() {
         <Routes>
           <Route path = "/" exact element={<Home />} />
           <Route path = "/learn" exact element={<Learn />} />
-          <Route path = "/contact" exact element={<Contact />} />
           <Route path = "/lessons" exact element={<Lessons />} />
           <Route path = "/translator" exact element={<Translator />} />
         </Routes>
