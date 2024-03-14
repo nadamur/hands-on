76bceec614a850c4e3f8162e08b33ef5f3c081fd
nadamur <nmurad4@uwo.ca>
2024-03-14 01:34:33 -0400

    lesson terms

--- src/pages/Lessons/Lesson.js
+++ src/pages/Lessons/Lesson.js
@@ -7,6 +7,7 @@ const lessonInfo = require('./Lessons.json');
 
 
 function Lesson() {
+    const [currentTermIndex, setCurrentTermIndex] = useState(0);
     const [terms, setTerms] = useState();
     const {lessonID} = useParams();
     useEffect(()=>{
@@ -15,11 +16,21 @@ function Lesson() {
       const lessonTitle = str[1];
       const lesson = lessonInfo[`lesson${lessonNum}`].find(lesson => lesson.title === lessonTitle);
       const terms = lesson ? lesson.terms : [];
-    }, [])
+      setTerms(terms);
+    }, [lessonID]);
+    const handleNextTerm = () => {
+      setCurrentTermIndex(prevIndex => (prevIndex + 1) % terms.length);
+    };
     return (
       <div className='lessons'>
         <Navbar/>
         <h1>ID: {lessonID}</h1>
+        {terms &&(
+          <div>
+            <p>Current Term: {terms[currentTermIndex]}</p>
+            <button onClick={handleNextTerm}>Next Term</button>
+          </div>
+        )}
         </div>
       );
 }
